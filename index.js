f0f35637b8f7b35b5c36b6d45c732d2fb6058baa
yin-sw <1076644979@qq.com>
2023-08-20 20:09:34 +0800

    mock 服务

---
+++ index.js
@@ -0,0 +1,27 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const mockList = require('./mock/index')
+
+const app = new Koa()
+const router = new Router()
+
+async function getRes(fn) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const res = fn()
+      resolve(res)
+    }, 1000);
+  })
+}
+
+mockList.forEach(item => {
+  const { url, method, response } = item
+  router[method](url, async ctx => {
+    // const res = response()
+    const res = await getRes(response)
+    ctx.body = res
+  })
+})
+
+app.use(router.routes())
+app.listen(3001)
---
+++ mock/question.js
@@ -0,0 +1,19 @@
+const Mock = require('mockjs')
+
+const Random = Mock.Random
+
+module.exports = [
+  {
+    url: '/api/question/:id',
+    method: 'get',
+    response() {
+      return {
+        errno: 0,
+        data: {
+          id: Random.id(),
+          title: Random.ctitle()
+        }
+      }
+    }
+  }
+]
